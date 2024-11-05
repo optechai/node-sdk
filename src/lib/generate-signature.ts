fa44ea0c1f8ff8a0f5329e0e9ff66b2e40b3e046
Alex Hinds <alex@optech.ai>
2024-11-05 16:39:22 +1100

    update signature genertion method

--- src/lib/generate-signature.ts
+++ src/lib/generate-signature.ts
@@ -2,8 +2,14 @@ import crypto from 'crypto'
 
 /**
  * Generate a signature for a given payload using a secret key.
+ *
+ * @param payload - The payload to sign - If no payload is provided, the signature will be generated for an empty string.
  */
-export function generateSignature(payload: Buffer | string, secret: string) {
+export function generateSignature(payload: Buffer | string | undefined, secret: string) {
+  if (!payload) {
+    return crypto.createHmac('sha256', secret).digest('base64').replace(/\+/g, '-').replace(/\//g, '_')
+  }
+
   return crypto
     .createHmac('sha256', secret)
     .update(payload)
