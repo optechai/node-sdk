115fd30d7b09be6d0bd3836d480fe9ae35c568ba
Alex Hinds <alex@optech.ai>
2024-11-07 15:19:38 +1100

    docs: add example

---
+++ examples/test.mjs
@@ -0,0 +1,22 @@
+import lorikeet from '@lorikeetai/node-sdk'
+
+const client = new lorikeet.Lorikeet()
+
+const customer = await client.customer.create({
+  firstName: 'John',
+  lastName: 'Doe',
+  displayName: 'John Doe',
+  email: 'john@test.com',
+  remoteId: '1234_definitely_legit',
+})
+
+const conversation = await client.conversation.chat.start({
+  customerId: customer.id,
+})
+
+const message = await client.conversation.chat.generate({
+  conversationId: conversation.conversationId,
+  message: 'Hi there, I was wondering if you could help me with something?',
+})
+
+console.log(message)
